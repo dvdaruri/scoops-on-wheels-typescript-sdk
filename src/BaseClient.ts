f4c5601f7d98c1fe5b84bc4f512075a9cda5297f
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2026-05-19 10:33:06 +0000

    SDK regeneration

--- src/BaseClient.ts
+++ src/BaseClient.ts
@@ -43,8 +43,8 @@ export function normalizeClientOptions<T extends BaseClientOptions = BaseClientO
         {
             "X-Fern-Language": "JavaScript",
             "X-Fern-SDK-Name": "scoops-on-wheels-dvdaruri",
-            "X-Fern-SDK-Version": "0.0.2",
-            "User-Agent": "scoops-on-wheels-dvdaruri/0.0.2",
+            "X-Fern-SDK-Version": "0.0.3",
+            "User-Agent": "scoops-on-wheels-dvdaruri/0.0.3",
             "X-Fern-Runtime": core.RUNTIME.type,
             "X-Fern-Runtime-Version": core.RUNTIME.version,
         },
